d6485296a9029a76f919a65423c6887e4c3934d2
Sriram-Kalyan3 <sriramkalyanyandra3@gmail.com>
2022-08-12 17:17:14 +0530

    Added the logic that enables to get data in the form of json objects. Need to add apis and connect with the local server.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-import './App.css'
+import {useState} from 'react';
 
 function HeaderTile(){
   return (
-    <header className="App text-3xl h-16 bg-blue-500 m-1 p-1 underline">
+    <header className="text-3xl h-16 bg-blue-500 m-1 p-1 underline">
       Money collect portal
     </header>
   )
 }
 
 function InputContainer(){
+  var clearData = {
+    name : '',
+    place : '',
+    amount : ''
+  }
+  let [formData, setFormData] = useState(clearData)
+  
+  function storeInDb(){
+    console.log(formData)
+    setFormData(clearData)
+  }
   return (
     <div className='m-1 bg-red-300 p-1'>
       <div className='m-1 '>
           <label htmlFor="personName" >Name of person : </label>
-          <input type="text" id="personName" placeholder="Name of person" />
+          <input type="text" id="personName" placeholder="Name of person" required
+            value={formData.name}
+            onChange={(event)=>{setFormData({...formData,name : event.target.value.trim()})}}/>
       </div>
     
       <div className='m-1 '>
           <label htmlFor="place">Place : </label>
-          <input type="text" id="place" placeholder="Place" />
+          <input type="text" id="place" placeholder="Place" 
+            value={formData.place}
+            onChange={(event)=>{setFormData({...formData,place : event.target.value.trim()})}}/>
       </div>
       
       <div className='m-1 '>
           <label htmlFor="amount">Amount : </label>
-          <input type="text" id="amount" placeholder="Amount" />
+          <input type="text" id="amount" placeholder="Amount" required
+            value={formData.amount}
+            onChange={(event)=>{
+              setFormData({...formData,amount : event.target.value.trim()})}}
+            onKeyUp ={(event)=>{
+              if(event.key === 'Enter'){
+                storeInDb()
+              }
+            }}              
+          />
+            
       </div>
 
       <div>
         <button className='m-1 bg-blue-400 p-1 rounded'
-          >
+          onClick={storeInDb}>
           Submit
           </button>
       </div>
