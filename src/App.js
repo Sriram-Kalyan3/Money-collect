3f20e8faee7ff231af0c695c6818ff3d0282d42c
Sriram-Kalyan3 <sriramkalyanyandra3@gmail.com>
2022-08-12 15:28:25 +0530

    Created the UI. Need to add background logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function HeaderTile(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App text-3xl h-16 bg-blue-500 m-1 p-1 underline">
+      Money collect portal
+    </header>
+  )
 }
 
-export default App;
+function InputContainer(){
+  return (
+    <div className='m-1 bg-red-300 p-1'>
+      <div className='m-1 '>
+          <label htmlFor="personName" >Name of person : </label>
+          <input type="text" id="personName" placeholder="Name of person" />
+      </div>
+    
+      <div className='m-1 '>
+          <label htmlFor="place">Place : </label>
+          <input type="text" id="place" placeholder="Place" />
+      </div>
+      
+      <div className='m-1 '>
+          <label htmlFor="amount">Amount : </label>
+          <input type="text" id="amount" placeholder="Amount" />
+      </div>
+
+      <div>
+        <button className='m-1 bg-blue-400 p-1 rounded'
+          >
+          Submit
+          </button>
+      </div>
+  
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+    <HeaderTile />
+    <InputContainer />
+    </>
+  )
+}
